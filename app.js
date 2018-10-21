eed4bbde5cd0e32f3aa0ae824447f11c32d2b61f
Cortez <cortezguitar@gmail.com>
2018-10-21 17:48:13 +0300

    done

---
+++ app.js
@@ -0,0 +1,51 @@
+const http = new easyHTTP();
+// Get posts
+// http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// // Get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', (err, post) => {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Create data
+const data = {
+  title: 'Custom post',
+  body: 'This is custom post'
+};
+
+// Create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, (err, post) => {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, (err, post) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', (err, response) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
+});
